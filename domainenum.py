3771151294130824181f383d3eec69c6f0a96978
liteman <romanokp@outlook.com>
2014-09-17 19:57:11 -0400

    Added brute-force from list of sub-domains-- in-progress

--- domainenum.py
+++ domainenum.py
@@ -4,12 +4,107 @@ from netaddr import *
 import argparse
 import sys
 import subprocess
+import os
+
+
+def list_all_domains(path):
+    """
+    List all domains
+    @param path: if it is a path, read the file and return a list of values,
+    if a string, return a list of one
+
+    """
+
+    domainlist = []
+    if os.path.exists(os.path.abspath(path)):
+        f = open(path, 'r')
+        for domain in f.readlines():
+            domainlist.append(domain.strip())
+        f.close()
+
+        return domainlist
+
+    else:
+        domainlist.append(path)
+    return domainlist
 
 def verifyDomain():
     '''
     #TODO -- perform some type of connectivity test to identify valid hosts
-    :return:
+    :return: Boolean specifying whether connectivity test was successful
     '''
+    return True
+
+def execute(cmdstr):
+
+    cmd = subprocess.Popen(cmdstr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return cmd.stdout.readline().decode("ASCII").rstrip()
+
+def domainLookup(domain, server=""):
+
+    #find A record with command: host <domain> <soa server> | grep "has address"
+    #Sample output: gmu.edu has address 129.174.1.38
+    arecord = execute("host " + domain + " " + server + " | grep 'has address'")
+    if arecord == '':
+        return "Not Found"
+    else:
+        return arecord.split()[-1]
+
+def findSOA(domain):
+    #find SOA for root: host -C <root domain> | grep -m 1 SOA
+    # sample output:
+    # gmu.edu has SOA record magda.gmu.edu. dnsadmin.gmu.edu. 2009036937 10800 3601 604800 86400
+    soa = execute("host -C " + domain + " | grep -m 1 SOA")
+    if soa == '':
+        return soa
+    else:
+        soa = soa.split()[4].rstrip(".")
+
+    return soa
+
+def bruteList(args):
+    '''
+
+    :param args: list of all command-line arguments
+    Requirement: at a minimum domain and wordlist must be supplied
+    :
+    '''
+
+    if not args.wordlist:
+        print "Error: No wordlist specified. Please use --wordlist"
+
+    if args.verbose:
+        print "Debug: Domain specified: " + args.domain
+        print "Debug: Wordlist specified: " + args.wordlist
+
+    rootlist = list_all_domains(args.domain)
+    sublist = list_all_domains(args.wordlist)
+
+    if args.verbose:
+        print "Debug: Domain(s): " + str(rootlist)
+        print "Debug: Sub-domains: " + str(sublist)
+
+    #loop through root domain list (--domain) and for each sub-domain look for valid A records
+    for root in rootlist:
+        resultDict = {}
+
+        soa = findSOA(root)  # find SOA for root domain
+        print root.upper() + " SOA is: " + soa
+
+        rootrecord = domainLookup(root, soa)  # find A record for root domain - use SOA name server if possible
+        print root.upper() + " address: " + rootrecord
+        resultDict[root] = rootrecord
+
+        for sub in sublist:
+            subdomain = sub + "." + root
+            subrecord = domainLookup(sub + "." + root, soa)
+            print "\t" + sub.upper() + "." + root.upper() + " address " + subrecord
+            resultDict[subdomain] = subrecord
+
+
+
+
+
 
 def bruteReverse(args):
     '''
@@ -24,9 +119,8 @@ def bruteReverse(args):
     ip = IPNetwork(ipnet)
     f = open("dnsenumResults.txt", 'w')
     for addr in ip:
-        cmd = subprocess.Popen("host " + str(addr), shell=True,
-                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        output = cmd.stdout.readline().decode("ASCII").rstrip()
+
+        output = execute("host " + str(addr))
 
         if "domain name pointer" in output:
             if not args.VERIFY:  #The user does not wish to verify connectivity
@@ -54,12 +148,8 @@ def main(argv):
                         metavar="IP/MASK",
                         required=False)
     parser.add_argument("--domain",
-                        help="Domain to enumerate (e.g google.com",
-                        metavar="DOM.TLD",
-                        required=False)
-    parser.add_argument("--domlist",
-                        help="Enumerate the specified list of domains",
-                        metavar="FILE",
+                        help="Enumerate the specified domain or list of domains",
+                        metavar="DOM.TLD or FILE",
                         required=False)
     parser.add_argument("--wordlist",
                         help="Brute-force domains based on predefined list",
@@ -71,7 +161,7 @@ def main(argv):
                              "the target domain(s)",
                         action="store_true",
                         required=False)
-    parser.add_argument("-v", "--VERBOSE",
+    parser.add_argument("-v", "--verbose",
                         help="Displays additional output",
                         action="store_true",
                         required=False)
@@ -79,10 +169,14 @@ def main(argv):
 
     args = parser.parse_args()
 
+    if args.verbose:
+        print "-v flag set. Debugging on.\n"
+
     if args.CIDR:
         bruteReverse(args)
 
-
+    if args.domain:
+        bruteList(args)
 
 
 main(sys.argv[1:])
